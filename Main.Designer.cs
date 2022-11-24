2012372657e6f4ee6198695030657688d12f38c9
Hevanafa <dhevansafa4@hotmail.com>
2022-11-24 09:29:01 +0700

    First Upload

---
+++ Main.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace ThaiIMEBasic
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbInput = new System.Windows.Forms.TextBox();
+            this.lbCandidates = new System.Windows.Forms.ListBox();
+            this.txbOutput = new System.Windows.Forms.TextBox();
+            this.cbAdvanced = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // txbInput
+            // 
+            this.txbInput.Location = new System.Drawing.Point(18, 18);
+            this.txbInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbInput.MaxLength = 5;
+            this.txbInput.Name = "txbInput";
+            this.txbInput.Size = new System.Drawing.Size(232, 27);
+            this.txbInput.TabIndex = 0;
+            this.txbInput.TextChanged += new System.EventHandler(this.txbInput_TextChanged);
+            this.txbInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txbInput_KeyDown);
+            this.txbInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbInput_KeyPress);
+            // 
+            // lbCandidates
+            // 
+            this.lbCandidates.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCandidates.FormattingEnabled = true;
+            this.lbCandidates.ItemHeight = 23;
+            this.lbCandidates.Location = new System.Drawing.Point(18, 56);
+            this.lbCandidates.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lbCandidates.Name = "lbCandidates";
+            this.lbCandidates.Size = new System.Drawing.Size(232, 211);
+            this.lbCandidates.TabIndex = 2;
+            // 
+            // txbOutput
+            // 
+            this.txbOutput.Location = new System.Drawing.Point(261, 18);
+            this.txbOutput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbOutput.Multiline = true;
+            this.txbOutput.Name = "txbOutput";
+            this.txbOutput.Size = new System.Drawing.Size(258, 249);
+            this.txbOutput.TabIndex = 1;
+            // 
+            // cbAdvanced
+            // 
+            this.cbAdvanced.AutoSize = true;
+            this.cbAdvanced.Location = new System.Drawing.Point(18, 297);
+            this.cbAdvanced.Name = "cbAdvanced";
+            this.cbAdvanced.Size = new System.Drawing.Size(403, 23);
+            this.cbAdvanced.TabIndex = 3;
+            this.cbAdvanced.Text = "&Advanced Orthography (Pali && Sanskrit orthography)";
+            this.cbAdvanced.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 332);
+            this.Controls.Add(this.cbAdvanced);
+            this.Controls.Add(this.txbOutput);
+            this.Controls.Add(this.lbCandidates);
+            this.Controls.Add(this.txbInput);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Basic Thai IME - By Hevanafa (23-11-2022)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbInput;
+        private System.Windows.Forms.ListBox lbCandidates;
+        private System.Windows.Forms.TextBox txbOutput;
+        private System.Windows.Forms.CheckBox cbAdvanced;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThaiIMEBasic
+{
+    public partial class Main : Form
+    {
+        Dictionary<string, string> dict, advancedDict;
+        string result;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            dict = new Dictionary<string, string>() {
+                { "b", "บ" },
+                { "c", "จ " },
+                { "ch", "ฉชฌ" },
+                { "d", "ฎด" },
+                { "f", "ฝฟ" },
+                { "k", "ก " },
+                { "kh", "ขฃคฅฆ"},
+                { "h", "หฮ"},
+                { "l", "ลฦฬ"},
+                { "m", "ม"},
+                { "n", "ณน" },
+                { "ng", "ง" },
+                { "ny", "ญญ"},
+                { "o", "อโ" },
+                { "p", "ป " },
+                { "ph", "ผพภ"},
+                { "r", "รฤ"},
+                //{ "rr", "รร" },
+                { "s", "ซศษส" },
+                { "t", "ฏต" },
+                { "th", "ฐฑฒถทธ"},
+                { "w", "ว"},
+                { "y", "ญย"},
+
+                // special case
+                { "q", "ๆ" },
+                { "-", "\u0e47" },
+
+                // vowels
+                { "a", "ะ\u0e31า" },
+                { "ae", "แ" },
+                { "am", "ำ" },
+                { "ai", "ใไ" },
+                { "e", "เ" },
+                { "i", "\u0e34\u0e35"},
+                { "ue", "\u0e36\u0e37ๅ"},
+                { "u", "\u0e38\u0e39"},
+
+                // tone marks
+                { "1", "\u0e48" },
+                { "2", "\u0e49" },
+                { "3", "\u0e4a" },
+                { "4", "\u0e4b" },
+                { "8", "\u0e47" },
+                { "9", "\u0e4c" },
+            };
+
+            advancedDict = new Dictionary<string, string>() {
+                { "b", "บพ" },
+                { "bh", "ภ" },
+                { "c", "จ " },
+                { "ch", "ฉ" },
+                { "d", "ฎฑดท" },
+                { "dh", "ฒธ" },
+                { "f", "ฟ" },
+                { "g", "ค " },
+                { "gg", "ฅ"}, // obsolete
+                { "gh", "ฆ" },
+                { "k", "ก " },
+                { "kh", "ข"},
+                { "h", "หฮ"},
+                { "l", "ลฦฬ"},
+                { "j", "ช "},
+                { "jh", "ฌ"},
+                { "m", "ม"},
+                { "n", "ณน" },
+                { "ng", "ง" },
+                { "ny", "ญ"},
+                { "o", "อโ" },
+                { "p", "ป " },
+                { "ph", "ผ"},
+                { "r", "รฤ"},
+                //{ "rr", "รร" },
+                { "s", "ศษส" },
+                { "t", "ฏต" },
+                { "th", "ฐถ"},
+                { "v", "ฝ" },
+                { "w", "ว"},
+                { "x", "ฃ"}, // obsolete
+                { "y", "ย"},
+                { "z", "ซ"},
+
+                // special case
+                { "q", "ๆ" },
+                { "-", "\u0e47" }, // mai-taikhu
+
+                // vowels
+                { "a", "ะ\u0e31า" },
+                { "ae", "แ" },
+                { "am", "ำ" },
+                { "ai", "ใไ" },
+                { "e", "เ" },
+                { "i", "\u0e34\u0e35"},
+                { "ue", "\u0e36\u0e37ๅ"},
+                { "u", "\u0e38\u0e39"},
+
+                // tone marks
+                { "1", "\u0e48" },
+                { "2", "\u0e49" },
+                { "3", "\u0e4a" },
+                { "4", "\u0e4b" },
+                { "8", "\u0e47" },
+                { "9", "\u0e4c" },
+            };
+        }
+
+        string trimmedResult {
+            get { return result?.Trim(); }
+        }
+
+        bool isAdvanced {
+            get { return cbAdvanced?.Checked ?? false; }
+        }
+
+        void updateList() {
+            lbCandidates.Items.Clear();
+            lbCandidates.SelectedIndex = -1;
+            var search = txbInput.Text.ToLower();
+
+            var d = isAdvanced ? advancedDict : dict;
+            result = d.ContainsKey(search)
+                ? d[search] : null;
+
+            if (result != null)
+            {
+                if (result.Length == 1) {
+                    appendOutput(result[0]);
+                    clearInput();
+                    return;
+                }
+
+                char c;
+                for (var a = 0; a < trimmedResult.Length; a++)
+                {
+                    c = result[a];
+                    lbCandidates.Items.Add($"{a + 1} - { c }");
+                }
+
+                lbCandidates.SelectedIndex = 0;
+            }
+        }
+
+        bool skipNext = false;
+        private void txbInput_TextChanged(object sender, EventArgs e)
+        {
+            if (skipNext) {
+                skipNext = false;
+                clearInput();
+                return;
+            }
+
+            updateList();
+        }
+
+        private void txbInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        void clearInput() {
+            txbInput.Text = "";
+        }
+
+        void appendOutput(char c) {
+            txbOutput.Text += c;
+        }
+
+        void insertOutput(char c) {
+            txbOutput.Text = txbOutput.Text.Insert(txbOutput.SelectionStart, $"{ c }");
+            txbOutput.SelectionStart++;
+        }
+
+        private void txbInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode) {
+                case Keys.Space:
+                    e.Handled = true;
+                    appendOutput(' ');
+                    break;
+                case Keys.Enter:
+                    // transfer input
+                    var idx = lbCandidates.SelectedIndex;
+                    if (idx >= 0)
+                    {
+                        appendOutput(result[idx]);
+                        clearInput();
+                    }
+
+                    break;
+                case Keys.Delete:
+                    clearInput();
+                    break;
+
+                case Keys.Up:
+                    lbCandidates.SelectedIndex--;
+                    break;
+                case Keys.Down:
+                    lbCandidates.SelectedIndex++;
+                    break;
+
+                case Keys k when k >= Keys.D0 && k <= Keys.D9:
+                    if (result?.Length > 0 && char.IsLetter(txbInput.Text[0]))
+                    {
+                        appendOutput(result[k - Keys.D1]);
+                        clearInput();
+                        skipNext = true;
+                    }
+
+                    break;
+            }
+        }
+    }
+}
